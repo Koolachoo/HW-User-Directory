67e67dfbb13c625772cf20172365f471f6782d01
Justin Taylor <kiradez211@gmail.com>
2020-06-20 23:10:23 -0400

    Table creation

--- userapp/src/App.js
+++ userapp/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Table from 'react-bootstrap/Table'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Table>
+    <thead>
+      <tr>
+        <th>Users</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Linda Cosco</td>
+      </tr>
+      <tr>
+        <td>Jerry Rice</td>
+      </tr>
+      <tr>
+        <td>Rock Stone</td>
+      </tr>
+      <tr>
+        <td>Bilbo Baggins</td>
+      </tr>
+      <tr>
+        <td>1</td>
+      </tr>
+    </tbody>
+  </Table>
 }
 
 export default App;
